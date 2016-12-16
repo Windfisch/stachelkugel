dbe38ad427ef1be9ec1b3350db8ef9c7b1ed0c38
Florian Jung <flo@windfisch.org>
2016-12-16 14:56:00 +0100

    .

---
+++ foo.js
@@ -0,0 +1,137 @@
+var gl;
+var vbo;
+var vPosAttr;
+
+
+function start()
+{
+	canvas = document.getElementById("glcanvas");
+	
+	try
+	{
+		gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
+	}
+	catch(e)
+	{
+		alert("webgl init failed");
+		gl = null;
+		return;
+	}
+
+
+	gl.clearColor(1.,0.,1.,1.);
+	gl.enable(gl.DEPTH_TEST);
+	gl.depthFunc(gl.LEQUAL);
+	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+	initShaders();
+	initBuffers();
+
+	drawScene();
+}
+
+function getShader(gl, id) {
+	var shaderScript = document.getElementById(id);
+
+	if (!shaderScript) {
+		return null;
+	}
+
+	var theSource = "";
+	var currentChild = shaderScript.firstChild;
+
+	while(currentChild) {
+		if (currentChild.nodeType == 3) {
+			theSource += currentChild.textContent;
+		}
+
+		currentChild = currentChild.nextSibling;
+	}
+
+	var shader;
+
+	if (shaderScript.type == "x-shader/x-fragment") {
+		shader = gl.createShader(gl.FRAGMENT_SHADER);
+	} else if (shaderScript.type == "x-shader/x-vertex") {
+		shader = gl.createShader(gl.VERTEX_SHADER);
+	} else {
+		return null;  // Unbekannter Shadertyp
+	}
+	gl.shaderSource(shader, theSource);
+
+	// Kompiliere das Shaderprogramm
+
+	gl.compileShader(shader);
+
+	// Überprüfe, ob die Kompilierung erfolgreich war
+
+	if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+		alert("Es ist ein Fehler beim Kompilieren der Shader aufgetaucht: " + gl.getShaderInfoLog(shader));
+		return null;
+	}
+
+	return shader;
+}
+
+function initShaders()
+{
+	var fragmentShader = getShader(gl, "shader-fs");
+	var vertexShader = getShader(gl, "shader-vs");
+
+	shaderProgram = gl.createProgram();
+	gl.attachShader(shaderProgram, vertexShader);
+	gl.attachShader(shaderProgram, fragmentShader);
+	gl.linkProgram(shaderProgram);
+
+	if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS))
+	{
+		alert("failed to link shader program" + gl.getProgramInfoLog(shaderProgram));
+	}
+
+	gl.useProgram(shaderProgram);
+
+	vPosAttr = gl.getAttribLocation(shaderProgram, "aVertexPosition");
+	gl.enableVertexAttribArray(vPosAttr);
+}
+
+function initBuffers()
+{
+	var vertices = [ 100.0, 100.0, -1.0,
+			 -100., 100., -1.,
+			 -100., -100., -1. ];
+	vbo = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+
+}
+
+
+function drawScene()
+{
+	gl.clearColor(1.,1.,0.,1.);
+	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+	var perspectiveMatrix = mat4.create();
+	mat4.perspective(perspectiveMatrix, 3.1415/4, 1, 1.0, 100.0);
+	console.log(perspectiveMatrix);
+	gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
+	gl.vertexAttribPointer(vPosAttr, 3, gl.FLOAT, false, 0, 0);
+
+
+	var mvMatrix = mat4.create();
+	mat4.identity(mvMatrix);
+
+	//setMatrixUniforms();
+	var pUniform = gl.getUniformLocation(shaderProgram, "uPMatrix");
+	gl.uniformMatrix4fv(pUniform, false, perspectiveMatrix);
+	
+	var mvUniform = gl.getUniformLocation(shaderProgram, "uMVMatrix");
+	gl.uniformMatrix4fv(mvUniform, false, mvMatrix);
+	
+	
+	gl.drawArrays(gl.TRIANGLES, 0, 3);
+}
+
+function setMatrixUniforms()
+{
+}
